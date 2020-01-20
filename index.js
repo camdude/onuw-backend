256c17251c1ca8a45083a3f96d81ac5dfba92233
Cameron <cc44@utas.edu.au>
2020-01-20 18:08:45 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+const port = process.env.PORT || 3000
+
+var app = express();
+app.use(express.json());
+
+app.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready On Server http://localhost:${port}`)
+});
+
+app.get("/get", (req, res, next) => {
+    res.json({
+        "version": process.env.VERSION
+    });
+});
+
+app.post('/post', function(request, response) {
+    response.send(request.body);
+});
